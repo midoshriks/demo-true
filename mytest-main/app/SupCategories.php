cc56667b518e1951eb9c53d33db4bed39b387d8b
midoshriks <midoshriks36@gmail.com>
2022-03-07 03:20:29 +0200

    update api

--- mytest-main/app/SupCategories.php
+++ mytest-main/app/SupCategories.php
@@ -8,4 +8,6 @@ class SupCategories extends Model
 {
     protected $fillable = ['name','code','cat_code'];
 
+
+
 }
--- mytest-main/routes/api.php
+++ mytest-main/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Category;
+use App\Product;
 use App\SupCategories;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/supcategories', function(){
     return SupCategories::all();
@@ -28,17 +30,52 @@ Route::get('/categories', function(){
     return response(['categories'=>Category::all()]);
 });
 
-Route::get('/categories/{id?}', function(){
+Route::get('/categories/{id}', function( $id){
+    $categories_id = Category::find($id);
+    // return response(['categories'=>Category::all()]);
+    return response(['categories'=>$categories_id]);
+});
+
+Route::get('/sub_categories/{id}', function( $id){
+    $sub_categories_id = SupCategories::find($id);
+    // return response(['categories'=>Category::all()]);
+    return response(['sub_categories'=>$sub_categories_id]);
+});
+
+Route::get('/categories/{id?}/{cat_code?}', function($id , $cat_code){
+
+    // return response([$cat_code = SupCategories::with('code', $cat_code)->where('id', $id)->get()]);
+
     // return category code
+    // return response(['cat_code'=>Category::find($id)]);
+
+
+    $cat = Category::find($id);
+    $cat_code = SupCategories::where('cat_code' , $cat_code )->get();
+
     // return subcategory where cat_code = id
+    // return response([$data]);
+
+    return response([
+        'category'=>$cat,
+        'sub_categories'=>$cat_code,
+    ]);
     // return response(['sub_categories'=>$var1, 'category'=>$var2]);
 });
 
 
 
-Route::get('/sub_categories/{id?}', function(){
+Route::get('/sub_categories/{id?}/{sub_category_code}', function($id , $sub_category_code){
     // $
     // return products where sub_category_code = id
     // return response(['products'=>$products]);
+
+    // $sub_category_code = Product::where('sub_category_code',$sub_category_code)->get();
+    $sub_categories_id = SupCategories::find($id);
+    $products = Product::where('sub_category_code',$sub_category_code)->get();
+    return response([
+        'id'=>$sub_categories_id,
+        'products'=>$products,
+    ]);
 });
 
